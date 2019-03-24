1a038bc083f50d9d2c30e49a70a1b4732fe5f2a4
hbruce48226 <holdenb5@gmail.com>
2019-03-24 13:26:36 -0400

    03.24.2019 push

--- experiment.js
+++ experiment.js
@@ -1,7 +1,13 @@
 let purchaseType = document.getElementById('purchase-type-dropdown'); //dropdown to choose entertainment, food, clothing, or bills
 let purchaseDescription = document.getElementById('purchase-information-description'); //input box to give description of purchase
+let purchaseDate = document.getElementById('purchase-information-date'); //input box to give date of purchase....automatically pulls today's date if left blank 
 let purchasePrice = document.getElementById('purchase-information-price'); //input box to give price of purchase
 
+
+let entertainmentSpentCounter = 0;
+let foodSpentCounter = 0;
+let clothingSpentCounter = 0;
+let billsSpentCounter = 0;
 
 
 // adds things to the ledger
@@ -17,14 +23,9 @@ class Ledger {
 
     display() {
         for (const item of this.ledgerItem) {
-            // const div = document.createElement('div'); //create a div for each pass
             const row = document.createElement('tr');
-            // const rowItem = document.createElement('td');
-
-            // const rowItemDescription = document.getElementById('')
 
             row.innerHTML = `
-            <td>${item.category}</td>
             <td>${item.description}</td>
             <td>${item.date}</td>
             <td>${item.price}</td>
@@ -38,25 +39,19 @@ class Ledger {
 
             if (purchaseType.value === 'Entertainment') {
                 ledger_table_entertainment.appendChild(row);
+                entertainmentSpentCounter += parseInt(purchasePrice.value);
             } else if (purchaseType.value === 'Food') {
                 ledger_table_food.appendChild(row);
+                foodSpentCounter += parseInt(purchasePrice.value);
             } else if (purchaseType.value === 'Clothing') {
                 ledger_table_clothing.appendChild(row);
+                clothingSpentCounter += parseInt(purchasePrice.value);
             } else if (purchaseType.value === 'Bills') {
                 ledger_table_bills.appendChild(row);
+                billsSpentCounter += parseInt(purchasePrice.value);
             }
-
-
         }
     }
-
-    // add(item) {
-    //     this.ledgerItem.push(item);
-    // }
-
-    // deleteAt(index) {
-    //     this.ledgerItem.splice(index, 1);
-    // }
 }
 
 
@@ -69,34 +64,14 @@ class LedgerItem {
     }
 }
 
-// finds the table element with id 'ledger-table' and assigns it to variable 'ledger_table'
-// const ledger_table = document.getElementById('ledger-table');
-
-//this is how we create (initialize) our new ledger
-// const ledger = new Ledger([
-//     new LedgerItem('Clothing', 'Jeans by Lucky', '03.18.2019', '$100'), 
-//     new LedgerItem('Clothing', 'T-shirt by Rag & Bone', '03.18.2019', '$60')
-// ]);
-// ledger.display(); //so the ledger actually shows up
-
-
-
-
-
-
-
-
 
 
 
 
 // this is the main header section: budget, amount spent, balance
 let userBudget = document.getElementById('userBudget'); //finds the main paragraph element with id 'userBudget' and assigns it to the variable 'userBudget'
-// userBudget = parseInt(userBudget);
 let amountSpent = document.getElementById('amountSpent'); //initializes amount spent with $0
-// amountSpent.innerHTML = 0; 
 let balance = document.getElementById('balance'); //set to 0 now but will be initialized to whatever the userBudget is 
-// balance = parseInt(balance);
 
 const addYourBudget = document.getElementById('submit-your-budget'); //finds the button with id 'submit-your-budget' and assigns to variable 'addYourBudget'
 // initializes the main header section
@@ -105,64 +80,76 @@ addYourBudget.addEventListener('click', () => {
     userBudget.innerHTML = `${parseInt(document.getElementById('what-is-your-budget').value)}`; //looks for value user gives, represents their budget
     amountSpent.innerHTML = `${parseInt(document.getElementById('what-is-your-budget').value)}` * 0; //this will be initialized with 0, no purchases made yet
     balance.innerHTML = `${parseInt(document.getElementById('what-is-your-budget').value)}`; //this will be initialized to whatever the budget is
-
-    console.log(`look at me bitch, the amountSpent is: ${amountSpent.innerHTML}`);
-    console.log(`Budget is ${typeof (userBudget)}`);
-    console.log(`Amount Spent is ${typeof (amountSpent)}`);
-    console.log(`Balance is ${typeof (balance)}`);
-
 });
-console.log(`look at me bitch, the amountSpent is: ${amountSpent.innerHTML}`);
 
 
 
 
-// this is the section for adding things to the ledger
-// let purchaseType = document.getElementById('purchase-type-dropdown'); //dropdown to choose entertainment, food, clothing, or bills
-// let purchaseDescription = document.getElementById('purchase-information-description'); //input box to give description of purchase
-// let purchasePrice = document.getElementById('purchase-information-price'); //input box to give price of purchase
 
 // addButton is how a new item/purchase is added to the ledger
 // the addButton, when clicked, calls the purchaseUpdater() and gets current userBudget and value of new item's price given in the input box by the user
 const addButton = document.getElementById('addButton'); //finds button with id 'addButton' and assigns to varaible 'addButton'
-// addButton.getAttribute('onclick',`purchaseUpdater(${amountSpent.innerHTML},${document.getElementById('purchase-information-price').value})`); //addButton calls the purchaseUpdater() function when clicked
-// addButton.getAttribute('onclick', `purchaseUpdater()`);
 
-
-
-// const inputPriceOfPurchase = document.getElementById('purchase-information-price'); //finds input value of purchase-information-price and assigns it to variable 'inputPriceOfPurchase'
-// addButton when clicked calls purchaseUpdater()
+// when clicked, addButton calls purchaseUpdater()
 addButton.addEventListener('click', () => {
-    console.log('button clicked');
-    let price = parseInt(purchasePrice.value);
-    // purchaseUpdater(balance,inputPriceOfPurchase.value);
-    purchaseUpdater(price);
-    console.log(amountSpent.innerHTML);
-    console.log(`look at me bitch, the amountSpent is: ${amountSpent.innerHTML}`);
 
-    // document.getElementById('balance').innerHTML = `your balance: ${balance}`; //displays the change in budget
+    //formats the date
+    const dateFormat = function () {
+        //if the date section is left blank, it will generate today's date in this specific format
+        if (purchaseDate.value === '') {
+            const rightNow = new Date();
+            let theDay = rightNow.getDate().toString();
+            let theMonth = rightNow.getMonth() + 1;
+            theMonth.toString();
+            let theYear = rightNow.getFullYear().toString();
+
+            if (parseInt(theMonth) < 10) {
+                theMonth = '0' + theMonth;
+            };
+            if (parseInt(theDay) < 10) {
+                theDay = '0' + theDay;
+            };
+            let stringDateFormat = theMonth + '.' + theDay + '.' + theYear;
+            return stringDateFormat;
+
+        } else { //if the user gives a date (like sometime in the past) then it will take whatever information it is given
+            return purchaseDate.value;
+        };
+    };
 
 
-    let newLedgerRow = new Ledger([new LedgerItem(`${purchaseType.value}`, `${purchaseDescription.value}`, `03.20.2019`, `$${purchasePrice.value}`)]);
-    newLedgerRow.display();
+
+
+
+
+    //alerts user if budget is spent
+    if (parseInt(balance.innerHTML) - purchasePrice.value >= 0) {
+        //update overview header section
+        let price = parseInt(purchasePrice.value);
+        purchaseUpdater(price);
+
+        //update the ledger
+        let newLedgerRow = new Ledger([new LedgerItem(`${purchaseType.value}`, `${purchaseDescription.value}`, `${dateFormat()}`, `$${purchasePrice.value}`)]);
+        newLedgerRow.display();
+
+    } else {
+        alert('you cannot make this purchase');
+        // const blackboxHeaderSection = document.getElementById('blackboxHeader');
+        // blackboxHeaderSection.style.backgroundColor = 'rgba(148, 0, 32, 0.6)';
+        // blackboxHeaderSection.style.borderRadius = '10px';
+        // blackboxHeaderSection.style.margin = '10px';
+        // const alertDiv = document.createElement('p');
+        // alertDiv.innerHTML = '~YOU CANNOT MAKE THIS PURCHASE~';
+        // alertDiv.style.marginTop = '-10px';
+        // alertDiv.style.color = 'white';
+        // blackboxHeaderSection.appendChild(alertDiv);
+        // document.getElementById('budget-input-section').style.marginTop = '0px';
+    };
+
+    purchaseDescription.value = ''; //clears the value after Submit
+    purchasePrice.value = ''; //clears the value after Submit
+
 });
-
-// // finds the table element with id 'ledger-table' and assigns it to variable 'ledger_table'
-// const ledger_table = document.getElementById('ledger-table');
-// //this is how we create (initialize) our new ledger
-// const ledger = new Ledger([
-//     new LedgerItem('Jeans by Lucky', '03.18.2019', '$100'), 
-//     new LedgerItem('T-shirt by Rag & Bone', '03.18.2019', '$60')
-// ]);
-// ledger.display(); //so the ledger actually shows up
-
-
-
-
-
-
-
-
 
 
 // purchaseUpdater() function is called when addButton is clicked
@@ -173,6 +160,31 @@ const purchaseUpdater = (itemPrice) => { //function takes in price of item and c
 
 
 
+// the spend breakdown section at the bottom of the page
+const spendBreakdownButton = document.getElementById('spendBreakdownButton');
+spendBreakdownButton.addEventListener('click', () => {
+    console.log('breakdown button pushed');
+    // redraw the breakdown piechart
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
 
+    function drawChart() {
 
+        let data = google.visualization.arrayToDataTable([
+            ['Purchase Type', 'Dollars Spent'],
+            ['Entertainment', entertainmentSpentCounter],
+            ['Food', foodSpentCounter],
+            ['Clothing', clothingSpentCounter],
+            ['Bills', billsSpentCounter]
+        ]);
 
+        const options = {
+            title: 'Monthly Spend Breakdown',
+            backgroundColor: 'red'
+        };
+
+        let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+    };
+});
